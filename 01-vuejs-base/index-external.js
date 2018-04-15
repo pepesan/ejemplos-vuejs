4306e1e3b49ed9577574fe2e433309db7252f789
pepesan <pepesan@gmail.com>
2018-04-15 13:53:35 +0200

    mejoras en la utilización de vue

---
+++ 01-vuejs-base/index-external.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",function(){
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Hola Mundo de VueJS'
+        }
+    });  
+});
+      
+    
