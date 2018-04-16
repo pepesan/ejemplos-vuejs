17a17c3e252d4a8ae3a31937e4b7fd52ae90a81a
pepesan <pepesan@gmail.com>
2018-04-17 00:36:25 +0200

    Colocada nueva ruta a componente About y navegación a distintas rutas en el menú principal

--- 08-vue-cli-base/src/App.vue
+++ 08-vue-cli-base/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </li>
+    </ul>
     <router-view/>
   </div>
 </template>
