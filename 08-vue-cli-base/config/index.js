7bfd49cea4b38d809c9ace621919f572a018a08e
pepesan <pepesan@gmail.com>
2018-04-17 22:48:34 +0200

    mejoras en el ejemplo de componentes

--- 08-vue-cli-base/config/index.js
+++ 08-vue-cli-base/config/index.js
@@ -44,8 +44,8 @@ module.exports = {
   },
 
   build: {
-    // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    // Template for index-componentes.html
+    index: path.resolve(__dirname, '../dist/index-componentes.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
